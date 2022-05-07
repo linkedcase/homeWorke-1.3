17f33517967e2f0e726c9cbfa451ec7e3893ba64
Белых Антон <belyh_anton@mail.ru>
2022-05-08 00:21:08 +0400

    Доработал задачу про подсчет комиссии

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,36 +1,36 @@
-const val MAX_SUM_OF_TRANSFERS = 75_000_00
+const val COMMISSION_MASTERCARD_MAESTRO = 0.006
+const val COMMISSION_VISA_MIR = 0.0075
 const val LIMIT_CARD_DAY = 150_000_00
 const val LIMIT_CARD_MONTH = 600_000_00
 const val LIMIT_VK_TRANSFER = 15_000_00
 const val LIMIT_VK_MONTH = 40_000_00
+const val MAX_SUM_OF_TRANSFERS = 75_000_00
 const val MINIMAL_COMMISSION_VISA_MIR = 35_00
 
-const val TYPE_CARD = "vkpay"
-const val AMOUNT_FOR_TRANSFER = 6_000_00
-var sumOfTransfersInMonth = 0
-
 fun main() {
-    val commission = calculateCommission(TYPE_CARD, AMOUNT_FOR_TRANSFER, sumOfTransfersInMonth)
+    val typeCard = "Vk Pay"
+    val sumOfTransfersInMonth = 0
+    val transfer = 6_000_00
 
-    printMessage(commission, TYPE_CARD, AMOUNT_FOR_TRANSFER)
+    val commission = calculateCommission(typeCard, sumOfTransfersInMonth, transfer)
+
+    printMessage(commission, typeCard, transfer, sumOfTransfersInMonth)
 }
 
 fun calculateCommission(
-    TYPE_CARD: String,
-    AMOUNT_FOR_TRANSFER: Int,
-    SUM_OF_TRANSFERS_IN_MONTH: Int,
-    commissionMastercardMaestro: Double = 0.006,
-    COMMISSION_VISA_MIR: Double = 0.0075
+    typeCard: String,
+    sumOfTransfersInMonth: Int,
+    transfer: Int
 ): Int {
-    val commission = when(TYPE_CARD) {
-        "mastercard", "maestro" -> {
-            if ((SUM_OF_TRANSFERS_IN_MONTH + AMOUNT_FOR_TRANSFER) < MAX_SUM_OF_TRANSFERS) {
+    val commission = when(typeCard) {
+        "Mastercard", "Maestro" -> {
+            if ((sumOfTransfersInMonth + transfer) < MAX_SUM_OF_TRANSFERS) {
                 0
             }
-            else (AMOUNT_FOR_TRANSFER * commissionMastercardMaestro).toInt() + 20_00
+            else (transfer * COMMISSION_MASTERCARD_MAESTRO).toInt() + 20_00
         }
-        "visa", "mir" -> {
-            val commission = (AMOUNT_FOR_TRANSFER * COMMISSION_VISA_MIR).toInt()
+        "Visa", "Мир" -> {
+            val commission = (transfer * COMMISSION_VISA_MIR).toInt()
             if (commission < MINIMAL_COMMISSION_VISA_MIR) {
                 MINIMAL_COMMISSION_VISA_MIR
             } else commission
@@ -42,50 +42,51 @@ fun calculateCommission(
 
 fun printMessage(
     commission: Int,
-    TYPE_CARD: String,
-    AMOUNT_FOR_TRANSFER: Int
+    typeCard: String,
+    transfer: Int,
+    sumOfTransfersInMonth: Int
 ) {
     when {
-        (TYPE_CARD == "vkpay" && AMOUNT_FOR_TRANSFER > LIMIT_VK_TRANSFER) ||
-                (TYPE_CARD == "vkpay" && (AMOUNT_FOR_TRANSFER + sumOfTransfersInMonth) > LIMIT_VK_TRANSFER)-> {
+        (typeCard == "Vk Pay" && transfer > LIMIT_VK_TRANSFER) ||
+                (typeCard == "Vk Pay" && (transfer + sumOfTransfersInMonth) > LIMIT_VK_TRANSFER)-> {
             val limit = LIMIT_VK_TRANSFER - sumOfTransfersInMonth
 
-            println("По счету $TYPE_CARD превышена максимальная сумма перевода за один раз.")
-            println("Перевод на сумму: ${AMOUNT_FOR_TRANSFER / 100} руб. " +
-                    "${AMOUNT_FOR_TRANSFER % 100} коп. отклонен.")
+            println("По счету $typeCard превышена максимальная сумма перевода за один раз.")
+            println("Перевод на сумму: ${transfer / 100} руб. " +
+                    "${transfer % 100} коп. отклонен.")
             println("Для перевода за один раз доступно: ${limit / 100} руб. ${limit % 100} коп.\"")
         }
 
-        TYPE_CARD == "vkpay" -> {
-            val limit = LIMIT_VK_MONTH - (sumOfTransfersInMonth + AMOUNT_FOR_TRANSFER)
+        typeCard == "Vk Pay" -> {
+            val limit = LIMIT_VK_MONTH - (sumOfTransfersInMonth + transfer)
 
-            println("Перевод со счета $TYPE_CARD на сумму: ${AMOUNT_FOR_TRANSFER / 100} руб. " +
-                    "${AMOUNT_FOR_TRANSFER % 100} коп.")
+            println("Перевод со счета $typeCard на сумму: ${transfer / 100} руб. " +
+                    "${transfer % 100} коп.")
             println("Размер комиссии: ${commission / 100} руб. ${commission % 100} коп.")
-            println("Доступный месячный лимит по счету $TYPE_CARD составляет" +
+            println("Доступный месячный лимит по счету $typeCard составляет" +
                     " ${limit / 100} руб. ${limit % 100} коп.")
         }
 
-        AMOUNT_FOR_TRANSFER > LIMIT_CARD_DAY || (sumOfTransfersInMonth + AMOUNT_FOR_TRANSFER) >
+        transfer > LIMIT_CARD_DAY || (sumOfTransfersInMonth + transfer) >
                 LIMIT_CARD_DAY -> {
             val limitDay = LIMIT_CARD_DAY - sumOfTransfersInMonth
 
-            println("Суточный лимит по карте $TYPE_CARD превышен.")
-            println("Перевод на сумму: ${AMOUNT_FOR_TRANSFER / 100} руб. " +
-                    "${AMOUNT_FOR_TRANSFER % 100} коп. отклонен.")
+            println("Суточный лимит по карте $typeCard превышен.")
+            println("Перевод на сумму: ${transfer / 100} руб. " +
+                    "${transfer % 100} коп. отклонен.")
             println("Доступный суточный лимит: ${limitDay / 100} руб. ${limitDay % 100} коп.")
         }
 
         else -> {
-            println("Перевод с карты $TYPE_CARD на сумму: ${AMOUNT_FOR_TRANSFER / 100} руб. " +
-                    "${AMOUNT_FOR_TRANSFER % 100} коп.")
+            println("Перевод с карты $typeCard на сумму: ${transfer / 100} руб. " +
+                    "${transfer % 100} коп.")
             println("Размер комиссии: ${commission / 100} руб. ${commission % 100} коп.")
 
-            val limitDay = LIMIT_CARD_DAY - (sumOfTransfersInMonth + AMOUNT_FOR_TRANSFER)
-            val limitMonth = LIMIT_CARD_MONTH - (sumOfTransfersInMonth + AMOUNT_FOR_TRANSFER)
+            val limitDay = LIMIT_CARD_DAY - (sumOfTransfersInMonth + transfer)
+            val limitMonth = LIMIT_CARD_MONTH - (sumOfTransfersInMonth + transfer)
 
-            println("По карте $TYPE_CARD доступны лимиты " +
-                    "Cуточный: ${limitDay / 100} руб. ${limitDay % 100} коп. " +
+            println("По карте $typeCard доступны лимиты " +
+                    "Суточный: ${limitDay / 100} руб. ${limitDay % 100} коп. " +
                     "Месячный: ${limitMonth / 100} руб. ${limitMonth % 100} коп.")
         }
     }
